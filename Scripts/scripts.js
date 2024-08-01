a91a8f40d1feed68ab15838dad01dfa0e06d4e1d
SergioLebron22 <lebronsergio22@gmail.com>
2024-07-31 21:53:58 -0400

    chore: Update HTML templates and fix login form

--- Scripts/scripts.js
+++ Scripts/scripts.js
@@ -1,8 +1,39 @@
-/* 
-  This is a SAMPLE FILE to get you started.
-  Please, follow the project instructions to complete the tasks.
-*/
 
 document.addEventListener('DOMContentLoaded', () => {
-  /* DO SOMETHING */
+  const loginForm = document.getElementById('login-form');
+
+  if (loginForm) {
+      loginForm.addEventListener('submit', async (event) => {
+          event.preventDefault();
+          // Your code to handle form submission
+
+          const email = document.querySelector('email');
+          const password = document.querySelector('password');
+
+          if (email === 'admin' && password === 'password'){
+            loginUser(email, password);
+          } 
+      });
+  }
 });
+
+async function loginUser(email, password) {
+  const response = await fetch('https://127.0.0.1:5000/login', {
+      method: 'POST',
+      headers: {
+          'Content-Type': 'application/json'
+      },
+      body: JSON.stringify({ email, password })
+  });
+  
+  if (response.ok) {
+      const data = await response.json();
+      document.cookie = `token=${data.access_token}; path=/`;
+      window.location.href = 'index.html';
+  } else {
+    alert('Login failed: ' + response.statusText);
+  }
+}
+
+
+
